3dae12493de9a1d1b02c14bfe85e5c09853769a4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-04 15:24:20 +0500

    product entity  writed

--- src/Baraka-Savdo.Domain/Entities/Products/Product.cs
+++ src/Baraka-Savdo.Domain/Entities/Products/Product.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Baraka_Savdo.Domain.Entities.Products
+﻿namespace Baraka_Savdo.Domain.Entities.Products
 {
     public class Product : Auditable
     {
