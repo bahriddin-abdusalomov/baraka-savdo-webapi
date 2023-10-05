709bd756844fcd15fc26bc56f12653ff8c327bd4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-05 13:44:05 +0500

    cotegory controller writed

--- src/Baraka-Savdo.WebApi/Controllers/CategoriesController.cs
+++ src/Baraka-Savdo.WebApi/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using Baraka_Savdo.Domain.Entities.Categories;
+﻿    using Baraka_Savdo.DataAccess.Utils;
+using Baraka_Savdo.Domain.Entities.Categories;
 using Baraka_Savdo.Service.Dtos.Categories;
 using Baraka_Savdo.Service.Interfaces.Categories;
 using Microsoft.AspNetCore.Http;
@@ -11,23 +12,36 @@ namespace Baraka_Savdo.WebApi.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _service;
+        private readonly int maxPageSize = 10;
 
         public CategoriesController(ICategoryService service)
         {
             this._service = service;
         }
 
-        [HttpPost]
-        public async Task<ActionResult> CreateAsync([FromForm] CategoryCreateDto category) => 
-            Ok(await _service.CreateAsync(category));
-
-        [HttpDelete]
-        public async Task<ActionResult> DeleteAsync(long categoryId) =>
-            Ok(await _service.DeleteAsync(categoryId));
-
         [HttpGet]
-        public async Task<IActionResult> CountAsync() => 
-            Ok(await _service.CountAsync());
+        public async Task<IActionResult> GetAllAsync([FromQuery] int page)
+            => Ok(await _service.GetAllAsync(new PaginationParams(page, maxPageSize)));
+
+        [HttpGet("{categoryId}")]
+        public async Task<IActionResult> GetByIdAsync(long id)
+            => Ok( await _service.GetByIdAsync(id));
+
+        [HttpGet("count")]
+        public async Task<IActionResult> CountAsync() 
+            => Ok(await _service.CountAsync());
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync([FromForm] CategoryCreateDto category) 
+            => Ok(await _service.CreateAsync(category));
+
+        [HttpPut("{categoryId}")]
+        public async Task<IActionResult> UpdateAsync(long id, [FromForm] CategoryUpdateDto category)
+            => Ok( await _service.UpdateAsync(id, category));
+
+        [HttpDelete("{categoryId}")]
+        public async Task<ActionResult> DeleteAsync(long categoryId) 
+            => Ok(await _service.DeleteAsync(categoryId));
         
     }
 }
