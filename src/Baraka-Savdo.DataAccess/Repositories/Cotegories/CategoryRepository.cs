416eaa9fe7b0ac997d762c1ebaa6aac85ef65bdc
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:29:21 +0500

    created category repository

--- src/Baraka-Savdo.DataAccess/Repositories/Cotegories/CategoryRepository.cs
+++ src/Baraka-Savdo.DataAccess/Repositories/Cotegories/CategoryRepository.cs
@@ -1,140 +1,133 @@
-﻿using Baraka_Savdo.DataAccess.Interfaces.Categories;
-using Baraka_Savdo.DataAccess.Utils;
-using Baraka_Savdo.Domain.Entities.Categories;
-using Dapper;
-using static Dapper.SqlMapper;
+﻿namespace Baraka_Savdo.DataAccess.Repositories.Cotegories;
 
-namespace Baraka_Savdo.DataAccess.Repositories.Cotegories
+public class CategoryRepository : BaseRepository, ICategoryRepository
 {
-    public class CategoryRepository : BaseRepository, ICategoryRepository
+    public async Task<long> CountAsync()
     {
-        public async Task<long> CountAsync()
+        try
         {
-            try
-            {
-                await _connection.OpenAsync();
-                string query = "Select count(*) From categories";
-                var result = await _connection.QuerySingleAsync<long>(query);
-                return result;
-            }
-            catch   
-            {
-                return 0;
-            }
-            finally
-            {
-                await _connection.CloseAsync();
-            }
-
+            await _connection.OpenAsync();
+            string query = "Select count(*) From categories";
+            var result = await _connection.QuerySingleAsync<long>(query);
+            return result;
+        }
+        catch   
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
         }
 
-        public async Task<int> CreateAsync(Category entity)
+    }
+
+    public async Task<int> CreateAsync(Category entity)
+    {
+        try
         {
-            try
-            {
-                await _connection.OpenAsync();
+            await _connection.OpenAsync();
 
-                string query = "INSERT INTO public.categories(name, description, image_path, created_at, updated_at)" +
-                    " VALUES (@Name, @Description, @ImagePath, @CreatedAt, @UpdatedAt);";
-                var result = await _connection.ExecuteAsync(query, entity);
-                return result;
-
-            }
-            catch
-            {
-                return 0;
-            }
-            finally
-            {
-                await _connection.CloseAsync();
-            }
-        }
-
-        public async Task<int> DeleteAsync(long id)
-        {
-            try
-            {
-                await _connection.OpenAsync();
-
-                string query = "Delete From categories Where id = @Id";
-                var result = await _connection.ExecuteAsync(query, new { Id = id });
-                return result;
-
-            }
-            catch
-            {
-                return 0;
-            }
-            finally
-            {
-                await _connection.CloseAsync();
-            }
-        }
-
-        public async Task<IList<Category>> GetAllAsync(PaginationParams @params)
-        {
-            try
-            {
-                await _connection.OpenAsync();
-
-                string query = $"Select * From categories order by id desc  " +
-                    $"offset {@params.GetSkipCount()} limit {@params.PageSize} ";
-
-                var result = (await _connection.QueryAsync<Category>(query)).ToList();
-                return result;
-
-            }
-            catch
-            {   
-                return new List<Category>();
-            }
-            finally
-            {
-                await _connection.CloseAsync();
-            }
-        }
-
-        public async Task<Category?> GetByIdAsync(long id)
-        {
-            try
-            {
-                await _connection.OpenAsync();
-                string query = "Select * From categories Where id = @Id";
-                var result = await _connection.QuerySingleAsync<Category>(query, new { Id = id });
-                return result;
-            }
-            catch
-            {
-                return null;
-            }
-            finally
-            {
-                await _connection.CloseAsync();
-            }
+            string query = "INSERT INTO public.categories(name, description, image_path, created_at, updated_at)" +
+                " VALUES (@Name, @Description, @ImagePath, @CreatedAt, @UpdatedAt);";
+            var result = await _connection.ExecuteAsync(query, entity);
+            return result;
 
         }
-
-        public async Task<int> UpdateAsync(long id, Category entity)
+        catch
         {
-            try
-            {
-                await _connection.OpenAsync();
-                string query = $"UPDATE public.categories " +
-                    $"SET name=@Name, description=@Description, image_path=@ImagePath, created_at=@CreatedAt, updated_at=@UpdatedAt " +
-                    $"WHERE id={id};";
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
 
-                var result = await _connection.ExecuteAsync(query, entity);
-                return result;
-            }
-            catch
-            {
-                return 0;
-            }
-            finally
-            {
-                await _connection.CloseAsync();
-            }
+    public async Task<int> DeleteAsync(long id)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = "Delete From categories Where id = @Id";
+            var result = await _connection.ExecuteAsync(query, new { Id = id });
+            return result;
 
         }
+        catch
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<IList<Category>> GetAllAsync(PaginationParams @params)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = $"Select * From categories order by id desc  " +
+                $"offset {@params.GetSkipCount()} limit {@params.PageSize} ";
+
+            var result = (await _connection.QueryAsync<Category>(query)).ToList();
+            return result;
+
+        }
+        catch
+        {   
+            return new List<Category>();
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<Category?> GetByIdAsync(long id)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = "Select * From categories Where id = @Id";
+            var result = await _connection.QuerySingleAsync<Category>(query, new { Id = id });
+            return result;
+        }
+        catch
+        {
+            return null;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+
+    }
+
+    public async Task<int> UpdateAsync(long id, Category entity)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = $"UPDATE public.categories " +
+                $"SET name=@Name, description=@Description, image_path=@ImagePath, created_at=@CreatedAt, updated_at=@UpdatedAt " +
+                $"WHERE id={id};";
+
+            var result = await _connection.ExecuteAsync(query, entity);
+            return result;
+        }
+        catch
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+
     }
 }
