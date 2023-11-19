b21805a5c0dc42add8735dd4db52178266ecbf5d
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:30:31 +0500

    add pagination meta data class

--- src/Baraka-Savdo.DataAccess/Utils/PaginationMetaData.cs
+++ src/Baraka-Savdo.DataAccess/Utils/PaginationMetaData.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Utils;
 
-namespace Baraka_Savdo.DataAccess.Utils
+public class PaginationMetaData
 {
-    public class PaginationMetaData
-    {
-        public bool HasPrevious { get; set; }
+    public bool HasPrevious { get; set; }
 
-        public bool HasNext { get; set; }
+    public bool HasNext { get; set; }
 
-        public int CurrentPage { get; set; }
+    public int CurrentPage { get; set; }
 
-        public int TotalPages { get; set; }
+    public int TotalPages { get; set; }
 
-        public int PageSize { get; set; }
+    public int PageSize { get; set; }
 
-        public int TotalItems { get; set; }
-    }
+    public int TotalItems { get; set; }
 }
