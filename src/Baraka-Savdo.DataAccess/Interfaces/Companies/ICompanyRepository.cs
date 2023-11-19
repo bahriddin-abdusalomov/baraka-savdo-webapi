4e870bc3ecb6493434c09d19d639a057ded2ea37
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:27:15 +0500

    created company interface

--- src/Baraka-Savdo.DataAccess/Interfaces/Companies/ICompanyRepository.cs
+++ src/Baraka-Savdo.DataAccess/Interfaces/Companies/ICompanyRepository.cs
@@ -1,14 +1,5 @@
-﻿using Baraka_Savdo.DataAccess.Common.Interfaces;
-using Baraka_Savdo.Domain.Entities.Companies;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Interfaces.Companies;
 
-namespace Baraka_Savdo.DataAccess.Interfaces.Companies
+public interface ICompanyRepository : IRepository<Company, Company>, IGetAll<Company>
 {
-    public interface ICompanyRepository : IRepository<Company, Company>, IGetAll<Company>
-    {
-    }
 }
