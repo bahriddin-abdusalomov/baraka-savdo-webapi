2faf4d39c7dc964d87ef310fcd21e18ae18940a8
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:30:56 +0500

    add pagination params class

--- src/Baraka-Savdo.DataAccess/Utils/PaginationParams.cs
+++ src/Baraka-Savdo.DataAccess/Utils/PaginationParams.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Utils;
 
-namespace Baraka_Savdo.DataAccess.Utils
+public class PaginationParams
 {
-    public class PaginationParams
+    public int PageNumber { get ; set; }
+    public int PageSize { get; set; }
+    public PaginationParams(int pageNumber, int pageSize)
     {
-        public int PageNumber { get ; set; }
-        public int PageSize { get; set; }
-        public PaginationParams(int pageNumber, int pageSize)
-        {
-            PageNumber = pageNumber;
-            PageSize = pageSize;
-        }
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+    }
 
-        public int GetSkipCount()
-        {
-            return (PageNumber - 1) * PageSize;
-        }
+    public int GetSkipCount()
+    {
+        return (PageNumber - 1) * PageSize;
     }
 }
