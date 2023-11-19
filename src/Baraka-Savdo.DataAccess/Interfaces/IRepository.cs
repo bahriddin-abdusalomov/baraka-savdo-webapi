27c3d4974e7320e9a28fc3790b3f6e9ac1612a05
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:29:03 +0500

    created base repository

--- src/Baraka-Savdo.DataAccess/Interfaces/IRepository.cs
+++ src/Baraka-Savdo.DataAccess/Interfaces/IRepository.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Interfaces;
 
-namespace Baraka_Savdo.DataAccess.Interfaces
+public interface IRepository<TEntity, TViewModel>
 {
-    public interface IRepository<TEntity, TViewModel>
-    {
-        public Task<int> CreateAsync(TEntity entity);
+    public Task<int> CreateAsync(TEntity entity);
 
-        public Task<int> UpdateAsync(long id, TEntity entity);
+    public Task<int> UpdateAsync(long id, TEntity entity);
 
-        public Task<int> DeleteAsync(long id);
+    public Task<int> DeleteAsync(long id);
 
-        public Task<TViewModel?> GetByIdAsync(long id);
+    public Task<TViewModel?> GetByIdAsync(long id);
 
-        public Task<long> CountAsync();
-    }
+    public Task<long> CountAsync();
 }
