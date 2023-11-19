cd43027fb6230d5caec8fefcf875a75293e06bae
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:27:58 +0500

    created product interface

--- src/Baraka-Savdo.DataAccess/Interfaces/Products/IProductRepository.cs
+++ src/Baraka-Savdo.DataAccess/Interfaces/Products/IProductRepository.cs
@@ -1,15 +1,6 @@
-﻿using Baraka_Savdo.DataAccess.Common.Interfaces;
-using Baraka_Savdo.DataAccess.ViewModels.Products;
-using Baraka_Savdo.Domain.Entities.Products;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Interfaces.Products;
 
-namespace Baraka_Savdo.DataAccess.Interfaces.Products
+public interface IProductRepository : IRepository<Product, ProductViewModel>,
+                                        IGetAll<ProductViewModel>, ISearchable<ProductViewModel>
 {
-    public interface IProductRepository : IRepository<Product, ProductViewModel>, IGetAll<ProductViewModel>, ISearchable<ProductViewModel>
-    {
-    }
 }
