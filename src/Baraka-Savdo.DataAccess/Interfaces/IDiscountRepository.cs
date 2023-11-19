8b0fa427c6b323e36be644d3127f11a04c5023f4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:28:45 +0500

    created discount interface

--- src/Baraka-Savdo.DataAccess/Interfaces/IDiscountRepository.cs
+++ src/Baraka-Savdo.DataAccess/Interfaces/IDiscountRepository.cs
@@ -1,14 +1,5 @@
-﻿using Baraka_Savdo.DataAccess.Common.Interfaces;
-using Baraka_Savdo.Domain.Entities.Discounts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Interfaces;
 
-namespace Baraka_Savdo.DataAccess.Interfaces
+public interface IDiscountRepository : IRepository<Discount, Discount>, IGetAll<Discount>
 {
-    public interface IDiscountRepository : IRepository<Discount, Discount>, IGetAll<Discount>
-    {
-    }
 }
