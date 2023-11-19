c133bae7d3ec7a800b25a85b07afbc558f73709e
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:26:50 +0500

    created category interface

--- src/Baraka-Savdo.DataAccess/Interfaces/Categories/ICategoryRepository.cs
+++ src/Baraka-Savdo.DataAccess/Interfaces/Categories/ICategoryRepository.cs
@@ -1,15 +1,5 @@
-﻿using Baraka_Savdo.DataAccess.Common.Interfaces;
-using Baraka_Savdo.Domain.Entities.Categories;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Interfaces.Categories;
 
-namespace Baraka_Savdo.DataAccess.Interfaces.Categories
+public interface ICategoryRepository : IRepository<Category, Category>, IGetAll<Category>
 {
-    public interface ICategoryRepository : IRepository<Category, Category>, IGetAll<Category>
-    {
-    }
 }
