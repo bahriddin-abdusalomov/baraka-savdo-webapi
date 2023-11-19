e06b7b5c1a31014a2a74f26d0a73ac602712ce7c
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:26:05 +0500

    created searchable interface

--- src/Baraka-Savdo.DataAccess/Common/Interfaces/ISearchable.cs
+++ src/Baraka-Savdo.DataAccess/Common/Interfaces/ISearchable.cs
@@ -1,14 +1,6 @@
-﻿using Baraka_Savdo.DataAccess.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Common.Interfaces;
 
-namespace Baraka_Savdo.DataAccess.Common.Interfaces
+public interface ISearchable<TModel>
 {
-    public interface ISearchable<TModel>
-    {
-        public Task<(int TModel, List<TModel>)> SearchAsync(string search, PaginationParams @params);
-    }
+    public Task<(int TModel, List<TModel>)> SearchAsync(string search, PaginationParams @params);
 }
