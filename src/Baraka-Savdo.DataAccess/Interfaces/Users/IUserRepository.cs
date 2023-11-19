538b055c028e6fe4b4356a137d23c8beeea9cd1c
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:28:12 +0500

    created user interface

--- src/Baraka-Savdo.DataAccess/Interfaces/Users/IUserRepository.cs
+++ src/Baraka-Savdo.DataAccess/Interfaces/Users/IUserRepository.cs
@@ -1,17 +1,7 @@
-﻿using Baraka_Savdo.DataAccess.Common.Interfaces;
-using Baraka_Savdo.DataAccess.ViewModels.Users;
-using Baraka_Savdo.Domain.Entities.Users;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Interfaces.Users;
 
-namespace Baraka_Savdo.DataAccess.Interfaces.Users
+public interface IUserRepository : IRepository<User, UserViewModel>,
+      IGetAll<UserViewModel>, ISearchable<UserViewModel>
 {
-    public interface IUserRepository : IRepository<User, UserViewModel>,
-          IGetAll<UserViewModel>, ISearchable<UserViewModel>
-    {
-        public Task<User?> GetByPhoneAsync(string phone);
-    }
+    public Task<User?> GetByPhoneAsync(string phone);
 }
