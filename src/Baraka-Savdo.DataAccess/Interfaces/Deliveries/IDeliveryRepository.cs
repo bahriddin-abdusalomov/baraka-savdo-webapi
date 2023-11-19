d8839d8646dbbf901512cbb9a5c5536f719c87f0
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-19 17:27:37 +0500

    created delivery interface

--- src/Baraka-Savdo.DataAccess/Interfaces/Deliveries/IDeliveryRepository.cs
+++ src/Baraka-Savdo.DataAccess/Interfaces/Deliveries/IDeliveryRepository.cs
@@ -1,16 +1,6 @@
-﻿using Baraka_Savdo.DataAccess.Common.Interfaces;
-using Baraka_Savdo.DataAccess.ViewModels.Deliveries;
-using Baraka_Savdo.Domain.Entities.Deliveries;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.DataAccess.Interfaces.Dileveries;
 
-namespace Baraka_Savdo.DataAccess.Interfaces.Dileveries
+internal interface IDeliveryRepository : IRepository<Delivery, Delivery>, IGetAll<DeliveryViewModel>
 {
-    internal interface IDeliveryRepository : IRepository<Delivery, Delivery>, IGetAll<DeliveryViewModel>
-    {
-        public Task<DeliveryViewModel> GetDeliverAsync(long id);
-    }
+    public Task<DeliveryViewModel> GetDeliverAsync(long id);
 }
