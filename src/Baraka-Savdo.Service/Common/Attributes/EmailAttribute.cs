771d53694174c8b266a962aa1d8ea7838ea04af4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-12 23:16:18 +0500

    created email attribute📧

--- src/Baraka-Savdo.Service/Common/Attributes/EmailAttribute.cs
+++ src/Baraka-Savdo.Service/Common/Attributes/EmailAttribute.cs
@@ -1,5 +1,24 @@
-﻿namespace Baraka_Savdo.Service.Common.Attributes;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class EmailAttribute : Attribute
+namespace ElitaShop.Services.CustomAttributes;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class EmailAttribute : ValidationAttribute
 {
+    public override bool IsValid(object value)
+    {
+        if (value == null)
+        {
+            return false;
+        }
+
+        string? email = value.ToString();
+
+        if (System.Text.RegularExpressions.Regex.IsMatch(email, @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$"))
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
