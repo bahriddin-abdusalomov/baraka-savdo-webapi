588f8701119e9518efa7c71d6b5d664b88efe112
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-12 23:16:58 +0500

    created phone number attribute☎️

---
+++ src/Baraka-Savdo.Service/Common/Attributes/PhoneAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Baraka_Savdo.Service.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class PhoneNumberAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value == null) return false;
+
+        string? phoneNumber = value.ToString();
+
+        if (System.Text.RegularExpressions.Regex.IsMatch(phoneNumber, @"^\+\d{12}$"))
+            return true;
+
+        return false;
+    }
+}
