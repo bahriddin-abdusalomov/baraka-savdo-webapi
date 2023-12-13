2706d8d2de69b4f6984172c89e3c43a9dfd371c0
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-14 02:11:59 +0500

    updated category service

--- src/Baraka-Savdo.Service/Interfaces/Categories/ICategoryService.cs
+++ src/Baraka-Savdo.Service/Interfaces/Categories/ICategoryService.cs
@@ -1,25 +1,16 @@
-﻿using Baraka_Savdo.DataAccess.Utils;
-using Baraka_Savdo.Domain.Entities.Categories;
-using Baraka_Savdo.Service.Dtos.Categories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace Baraka_Savdo.Service.Interfaces.Categories
+﻿namespace Baraka_Savdo.Service.Interfaces.Categories;
+
+public interface ICategoryService
 {
-    public interface ICategoryService
-    {
-        public Task<bool> CreateAsync(CategoryCreateDto dto);
+    public Task<bool> CreateAsync(CategoryCreateDto dto);
 
-        public Task<bool> DeleteAsync(long categoryId);
+    public Task<bool> DeleteAsync(long categoryId);
 
-        public Task<long> CountAsync();
+    public Task<long> CountAsync();
 
-        public Task<IList<Category>> GetAllAsync(PaginationParams @params);
+    public Task<Category> GetByIdAsync(long categoryId);
+    
+    public Task<IList<Category>> GetAllAsync(PaginationParams @params);
 
-        public Task<Category> GetByIdAsync(long categoryId);
-
-        public Task<bool> UpdateAsync(long categoryId, CategoryUpdateDto dto);
-    }
+    public Task<bool> UpdateAsync(long categoryId, CategoryUpdateDto dto);
 }
