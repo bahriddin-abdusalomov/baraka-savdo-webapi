aeaa0ac9537f9ad810fbf167760e94d116767353
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-14 00:31:13 +0500

    udpated category dto

--- src/Baraka-Savdo.Service/Dtos/Categories/CategoryCreateDto.cs
+++ src/Baraka-Savdo.Service/Dtos/Categories/CategoryCreateDto.cs
@@ -1,19 +1,10 @@
-﻿using Baraka_Savdo.Domain.Entities.Categories;
-using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Baraka_Savdo.Service.Dtos.Categories;
 
-namespace Baraka_Savdo.Service.Dtos.Categories
+public class CategoryCreateDto 
 {
-    public class CategoryCreateDto 
-    {
-        public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
-        public string Description { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 
-        public IFormFile ImagePath { get; set; } = default!;
-    }
+    public IFormFile ImagePath { get; set; } = default!;
 }
