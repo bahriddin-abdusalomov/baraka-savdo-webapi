22a621bf172d18afe9e2fc5d0181611cdf293fdc
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-14 00:11:37 +0500

    created user repository🙎

---
+++ src/Baraka-Savdo.DataAccess/Repositories/Users/UserRepository.cs
@@ -0,0 +1,213 @@
+﻿namespace Baraka_Savdo.DataAccess.Repositories.Users;
+
+public class UserRepository : BaseRepository, IUserRepository
+{
+    public async Task<long> CountAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = "Select count(*) From users";
+            var result = await _connection.QuerySingleAsync<long>(query);
+
+            return result;
+        }
+        catch
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+
+    }
+    public async Task<int> CreateAsync(User entity)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = "INSERT INTO public.users(" +
+                            "first_name, last_name, phone_number, " +
+                            "phone_number_confirmed, passport_seria_number, " +
+                            "is_male, birth_date, country, region, password_hash, salt, " +
+                            "image_path, last_activity, identity_role, created_at, updated_at) " +
+                            "VALUES (@FirstName, @LastName, @PhoneNumber, @PhoneNumberConfirmed, @PassportSeriaNumber, " +
+                            "@IsMale, @BirthDate, @Country, @Region, @PasswordHash, @Salt, @ImagePath, " +
+                            "@LastActivity, @IdentityRole, @CreatedAt, @UpdatedAt);";
+
+            var result = await _connection.ExecuteAsync(query, entity);
+            return result;
+
+        }
+        catch
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<int> DeleteAsync(long id)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = "Delete From users Where id = @Id";
+            var result = await _connection.ExecuteAsync(query, new { Id = id });
+            return result;
+
+        }
+        catch
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<IList<UserViewModel>> GetAllAsync(PaginationParams @params)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = $"Select * From users order by id desc  " +
+                $"offset {@params.GetSkipCount()} limit {@params.PageSize} ";
+
+            var result = (await _connection.QueryAsync<UserViewModel>(query)).ToList();
+            return result;
+
+        }
+        catch
+        {
+            return new List<UserViewModel>();
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<UserViewModel?> GetByIdAsync(long id)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = "Select * From users Where id = @Id";
+            var result = await _connection.QuerySingleAsync<UserViewModel>(query, new { Id = id });
+            return result;
+        }
+        catch
+        {
+            return null;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<IList<UserViewModel>?> GetByPhoneAsync(string phone)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = "SELECT * " +
+               "FROM users " +
+               "WHERE phone_number LIKE '%' + @searchPhoneNumber + '%';";
+
+            var result = (await _connection.QueryAsync<UserViewModel>(query, new { searchPhoneNumber = phone })).ToList();
+
+            return result;
+        }
+        catch
+        {
+            return null;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<(int TModel, List<UserViewModel>)> SearchAsync(string search, PaginationParams @params)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = "SELECT * " +
+               "FROM users " +
+               "WHERE " +
+               "    first_name LIKE '%' + @searchTerm + '%' OR " +
+               "    last_name LIKE '%' + @searchTerm + '%' OR " +
+               "    phone_number LIKE '%' + @searchTerm + '%' OR " +
+               "    passport_seria_number LIKE '%' + @searchTerm + '%' OR " +
+               "    passport_seria_confirmed LIKE '%' + @searchTerm + '%' OR " +
+               "    birth_date LIKE '%' + @searchTerm + '%' OR " +
+               "    is_male LIKE '%' + @searchTerm + '%' OR " +
+               "    country LIKE '%' + @searchTerm + '%' OR " +
+               "    region LIKE '%' + @searchTerm + '%';";
+
+            var result = (await _connection.QueryAsync<UserViewModel>(query, new { searchTerm = search })).ToList();
+
+            return (@params.PageNumber, result);
+        }
+        catch
+        {
+            return (@params.PageNumber, new List<UserViewModel>());
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<int> UpdateAsync(long id, User entity)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+
+            string query = "UPDATE public.users " +
+                     "SET first_name = @FirstName, " +
+                     "    last_name = @LastName, " +
+                     "    phone_number = @PhoneNumber, " +
+                     "    phone_number_confirmed = @PhoneNumberConfirmed, " +
+                     "    passport_seria_number = @PassportSeriaNumber, " +
+                     "    is_male = @IsMale, " +
+                     "    birth_date = @BirthDate, " +
+                     "    country = @Country, " +
+                     "    region = @Region, " +
+                     "    password_hash = @PasswordHash, " +
+                     "    salt = @Salt, " +
+                     "    image_path = @ImagePath, " +
+                     "    last_activity = @LastActivity, " +
+                     "    identity_role = @IdentityRole, " +
+                     "    updated_at = @UpdatedAt " +
+                     "WHERE id = @Id;";
+
+            entity.Id = id;
+            var result = await _connection.ExecuteAsync(query, entity);
+
+            return result;
+        }
+        catch
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+}
