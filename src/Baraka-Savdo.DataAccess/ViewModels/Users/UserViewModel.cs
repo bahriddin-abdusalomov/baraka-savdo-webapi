987ee7d22af5007b7d20fdfd61473b8e56d9de10
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-14 00:17:42 +0500

    created user view model🙎

--- src/Baraka-Savdo.DataAccess/ViewModels/Users/UserViewModel.cs
+++ src/Baraka-Savdo.DataAccess/ViewModels/Users/UserViewModel.cs
@@ -2,4 +2,18 @@
 
 public class UserViewModel
 {
+    [MaxLength(50)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [MaxLength(50)]
+    public string LastName { get; set; } = string.Empty;
+
+    public bool IsMale { get; set; }
+
+    public string ImagePath { get; set; } = string.Empty;
+
+    [MaxLength(13)]
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public DateTime LastActivity { get; set; }
 }
