25778402ec98299fbf13b157fa77a71f502c061d
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-25 12:47:33 +0500

    created companies controller

---
+++ src/Baraka-Savdo.WebApi/Controllers/CompaniesController.cs
@@ -0,0 +1,48 @@
+﻿using Baraka_Savdo.DataAccess.Utils;
+using Baraka_Savdo.Service.Dtos.Categories;
+using Baraka_Savdo.Service.Dtos.Companies;
+using Baraka_Savdo.Service.Interfaces.Categories;
+using Baraka_Savdo.Service.Interfaces.Companies;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baraka_Savdo.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyService _service;
+        private readonly int maxPageSize = 10;
+
+        public CompaniesController(ICompanyService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync([FromQuery] int page)
+            => Ok(await _service.GetAllAsync(new PaginationParams(page, maxPageSize)));
+
+        [HttpGet]
+        public async Task<IActionResult> GetByIdAsync(long id)
+            => Ok(await _service.GetByIdAsync(id));
+
+        [HttpGet]
+        public async Task<IActionResult> CountAsync()
+            => Ok(await _service.CountAsync());
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync([FromForm] CompanyCreateDto company)
+            => Ok(await _service.CreateAsync(company));
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(long id, [FromForm] CompanyUpdateDto company)
+            => Ok(await _service.UpdateAsync(id, company));
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteAsync(long companyId)
+            => Ok(await _service.DeleteAsync(companyId));
+    }
+}
