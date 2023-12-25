10739c45d3f98a1bff62ab2826b63345cef793cc
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-25 12:49:41 +0500

    created company service inteface

---
+++ src/Baraka-Savdo.Service/Interfaces/Companies/ICompanyService.cs
@@ -0,0 +1,19 @@
+﻿using Baraka_Savdo.Domain.Entities.Companies;
+using Baraka_Savdo.Service.Dtos.Companies;
+
+namespace Baraka_Savdo.Service.Interfaces.Companies;
+
+public interface ICompanyService
+{
+    public Task<bool> CreateAsync(CompanyCreateDto dto);
+
+    public Task<bool> DeleteAsync(long companyId);
+
+    public Task<long> CountAsync();
+
+    public Task<Company> GetByIdAsync(long companyId);
+
+    public Task<IList<Company>> GetAllAsync(PaginationParams @params);
+
+    public Task<bool> UpdateAsync(long companyId, CompanyUpdateDto dto);
+}
