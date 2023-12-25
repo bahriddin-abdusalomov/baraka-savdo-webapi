9e4db9ebeee6559d0179d27ff5d4fac1fb6d2fe2
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-12-25 12:48:47 +0500

    created company dto

---
+++ src/Baraka-Savdo.Service/Dtos/Companies/CompanyUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Baraka_Savdo.Service.Dtos.Companies;
+
+public class CompanyUpdateDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public IFormFile? ImagePath { get; set; } 
+
+    public string PhoneNumber { get; set; } = string.Empty;
+}
