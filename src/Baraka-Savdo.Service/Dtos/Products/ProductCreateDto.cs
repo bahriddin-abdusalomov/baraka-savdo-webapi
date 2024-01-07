bae81c2d2416e0aac8fdabac273d548a0376031e
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-07 23:32:06 +0500

    created product dto

---
+++ src/Baraka-Savdo.Service/Dtos/Products/ProductCreateDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Baraka_Savdo.Service.Dtos.Products;
+
+public class ProductCreateDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public IFormFile ImagePath { get; set; } = default!;
+
+    public double UnitPrice { get; set; }
+
+    public long CategoryId { get; set; }
+
+    public long CompanyId { get; set; }
+}
---
+++ src/Baraka-Savdo.Service/Dtos/Products/ProductUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Baraka_Savdo.Service.Dtos.Products;
+
+public class ProductUpdateDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public IFormFile ImagePath { get; set; }
+
+    public double UnitPrice { get; set; }
+
+    public long CategoryId { get; set; }
+
+    public long CompanyId { get; set; }
+}
