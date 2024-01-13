286f5117eacb72128dc5e1d35a94db0d8a044afc
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-14 01:32:13 +0500

    updated

--- src/Baraka-Savdo.Domain/Entities/Categories/Category.cs
+++ src/Baraka-Savdo.Domain/Entities/Categories/Category.cs
@@ -12,7 +12,7 @@ namespace Baraka_Savdo.Domain.Entities.Categories
         [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
 
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } 
 
         public string ImagePath { get; set; } = string.Empty;
     }
