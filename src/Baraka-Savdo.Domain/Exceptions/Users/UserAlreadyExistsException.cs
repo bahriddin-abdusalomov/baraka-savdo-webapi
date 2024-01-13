2698e9ec6ed14828efcd0b1bdd1d43f2f938d19f
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-14 01:33:06 +0500

    created

---
+++ src/Baraka-Savdo.Domain/Exceptions/Users/UserAlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace Baraka_Savdo.Domain.Exceptions.Users;
+
+public class UserAlreadyExistsException : AlreadyExistsException
+{
+    public UserAlreadyExistsException()
+    {
+        TitleMessage = "User already !";
+    }
+}
