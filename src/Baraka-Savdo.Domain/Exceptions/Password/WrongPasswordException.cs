5a524fe25d232e70116b76ef918d2d1b7f70c91b
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-14 01:32:57 +0500

    created

---
+++ src/Baraka-Savdo.Domain/Exceptions/Password/WrongPasswordException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baraka_Savdo.Domain.Exceptions.Password;
+
+public class WrongPasswordException : NotFoundException
+{
+    public WrongPasswordException()
+    {
+        TitleMessage = "Wrong password !";
+    }
+}
