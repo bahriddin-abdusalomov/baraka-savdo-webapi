cf8d271a75e6dd723861220da5e7173a76910e2b
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-14 01:34:47 +0500

    created

---
+++ src/Baraka-Savdo.WebApi/Controllers/AuthorizationController.cs
@@ -0,0 +1,25 @@
+﻿using Baraka_Savdo.Service.Dtos.Auth;
+using Baraka_Savdo.Service.Interfaces.Auth;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baraka_Savdo.WebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AuthorizationController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthorizationController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterAsync(RegisterDto registerDto)
+    {
+        var result = await _authService.RegisterAsync(registerDto);
+        return Ok(result);
+    }
+}
