fc32b416f5f378af84e84b9956eaa58488807d4e
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-17 01:13:03 +0500

    created

---
+++ src/Baraka-Savdo.WebApi/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Baraka_Savdo.WebApi.Configurations;
+
+public static class SwaggerConfiguration
+{
+    public static void SwaggerConfigure(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Baraka Savdo Api",
+                Version = "v1"
+            });
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Bearer {token}",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }});
+        });
+    }
+}
