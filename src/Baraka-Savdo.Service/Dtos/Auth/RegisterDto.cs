aa11c95413d4da6ca37ecc24a4729575aad1745d
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-18 21:52:05 +0500

    commit

--- src/Baraka-Savdo.Service/Dtos/Auth/RegisterDto.cs
+++ src/Baraka-Savdo.Service/Dtos/Auth/RegisterDto.cs
@@ -2,8 +2,8 @@
 
 public class RegisterDto
 {
-    public string? FirstName { get; set; } 
-    public string? LastName { get; set; } 
-    public string? PhoneNumber {  get; set; } 
-    public string? Password { get; set; }
+    public string FirstName { get; set; } 
+    public string LastName { get; set; } 
+    public string PhoneNumber {  get; set; } 
+    public string Password { get; set; }
 }
