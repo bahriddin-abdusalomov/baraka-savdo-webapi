5ff936e55219469ddb12d02ea9a1a5a5ae585620
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-03-30 21:43:45 +0500

    updated

--- src/Baraka-Savdo.Service/Dtos/Auth/ResetPasswordDto.cs
+++ src/Baraka-Savdo.Service/Dtos/Auth/ResetPasswordDto.cs
@@ -2,6 +2,6 @@
 
 public class ResetPasswordDto
 {
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
